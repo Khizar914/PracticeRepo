0e79d62830161a10b98b4cec964b27958b53aa1e
00Nauman00 <buttn1@go.stockton.edu>
2026-02-10 01:46:53 +0000

    commit 1

--- reposit/ray.java
+++ reposit/ray.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 
 public class ray {
+    public static void mian (String [] args) {
+        System.out.println("This is Nauman.Today is Monday 09 fed, 2026.");
+    }
+    -
 
 
 }
